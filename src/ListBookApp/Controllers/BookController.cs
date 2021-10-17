bbcad110786ec87a06f6d089f2e41ee7fd2f51bd
Jarr59 <rrjosearr@hotmail.com>
2021-10-17 17:45:43 -0600

    CRUD book

--- src/ListBookApp/Controllers/BookController.cs
+++ src/ListBookApp/Controllers/BookController.cs
@@ -30,5 +30,63 @@ namespace ListBookApp.Controllers
             }
             return View(book);
         }
+        public IActionResult Edit([Bind]int? id)
+        {
+            if(id is null)
+            {
+                return NotFound();
+            }
+            var book = _service.GetById(id.Value);
+            if(book is null)
+            {
+                return NotFound();
+            }
+            return View(new EditBook{
+                IdBook = book.IdBook,
+                Name = book.Name,
+                ISBN =  book.ISBN
+            });
+        }
+        [HttpPost]
+        public IActionResult Edit(EditBook book)
+        {
+            if(ModelState.IsValid)
+            {
+                _service.Edit(book,book.IdBook);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+        public IActionResult Delete([Bind]int? id)
+        {
+            if(id is null)
+            {
+                return NotFound();
+            }
+            var book = _service.GetById(id.Value);
+            if(book is null)
+            {
+                return NotFound();
+            }
+            return View(new DeleteBook{
+                IdBook = book.IdBook,
+                Name = book.Name,
+                ISBN =  book.ISBN
+            });
+        }
+        [HttpPost]
+        public IActionResult Delete(DeleteBook book)
+        {
+            if(book == null)
+            {
+                return NotFound();
+            }
+            if(book.IdBook <= 0)
+            {
+                return NotFound();
+            }
+            _service.Delete(book.IdBook);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
