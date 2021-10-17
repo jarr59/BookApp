787ec17637395746d83908ba5f2c8f82152b1b28
Jarr59 <rrjosearr@hotmail.com>
2021-10-17 10:50:29 -0600

    View Create and List book

--- src/ListBookApp/Models/Book/CreateBook.cs
+++ src/ListBookApp/Models/Book/CreateBook.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ListBookApp.Models.Book
 {
     public class CreateBook
     {
+        [Display(Name = "Name")]
+        [Required]
+        [MaxLength(200)]
         public string Name { get; set; }
+        [Display(Name = "ISNB")]
+        [Required]
+        [MaxLength(16)]
         public string ISBN { get; set; }   
     }
 }
